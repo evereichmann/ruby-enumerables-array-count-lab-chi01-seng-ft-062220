a6a059647032429928ebe14f391b2ab0e7128d0a
Eve Reichmann <ereichmann4@aol.com>
2020-06-03 16:35:35 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,11 @@
 def count_strings(array)
-  array.count {|element|}
+  array.count do |element|
     element.class == String
   end
 end
 
 def count_empty_strings(array)
-  array.count {|element|}
+  array.count do |element|
     element.class == " "
   end
 end
